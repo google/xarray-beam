2106df630673aa5d4e8c153e107932ec48a0e959
Alex Merose <alxr@google.com>
2022-12-15 17:57:16 -0800

    rm future annotations import for now.

--- xarray_beam/_src/core.py
+++ xarray_beam/_src/core.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Core data model for xarray-beam."""
-from __future__ import annotations
-
 import itertools
 import math
 from typing import (
